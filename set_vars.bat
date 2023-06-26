34378966c94540d69f13173e24df217d5e0aa648
Arthur Mitchell <arthur@mitchell.coffee>
2023-06-26 19:02:55 +0100

    Update variables for windows implementations

--- set_vars.bat
+++ set_vars.bat
@@ -1,5 +1,3 @@
-set AWS_CONFIG_PATH=~\Documents\Software\AWS\Amazon_AWS_DDNS_Client\config\
-
 export HEALTHCHECK_HEARTBEAT_FILE=~/Documents/Software/AWS/Amazon_AWS_DDNS_Client/config/heartbeat
 export HEALTHCHECK_INTERVAL_FILE=~/Documents/Software/AWS/Amazon_AWS_DDNS_Client/config/max_age
 export AWS_ACCESS_KEY_ID=AAAAAAAAAAAAAAAAAAAAAA
@@ -10,4 +8,10 @@ export AWS_SHARED_CREDENTIALS_FILE=~/Documents/Software/AWS/Amazon_AWS_DDNS_Clie
 export AWS_ACCESS_KEY_ID_FILE=~/Documents/Software/AWS/Amazon_AWS_DDNS_Client/config/secret_aws_access_key_id
 export AWS_SECRET_ACCESS_KEY_FILE=~/Documents/Software/AWS/Amazon_AWS_DDNS_Client/config/secret_aws_secret_access_key
 
-$env:AWS_CONFIG_PATH = 'C:\Users\Arthur\Documents\Software\Github\Amazon_AWS_DDNS_Client\config'
+$env:AWS_CONFIG_PATH='C:\Users\Arthur\Documents\Software\Github\Amazon_AWS_DDNS_Client\config'
+$env:HEALTHCHECK_HEARTBEAT_FILE='C:\Users\Arthur\Documents\Software\Github\Amazon_AWS_DDNS_Client\config\heartbeat'
+$env:HEALTHCHECK_HEARTBEAT_FILE='C:\Users\Arthur\Documents\Software\Github\Amazon_AWS_DDNS_Client\config\max_age'
+
+set AWS_CONFIG_PATH=C:\Users\Arthur\Documents\Software\Github\Amazon_AWS_DDNS_Client\config
+set HEALTHCHECK_HEARTBEAT_FILE=C:\Users\Arthur\Documents\Software\Github\Amazon_AWS_DDNS_Client\config\heartbeat
+set HEALTHCHECK_HEARTBEAT_FILE=C:\Users\Arthur\Documents\Software\Github\Amazon_AWS_DDNS_Client\config\max_age
